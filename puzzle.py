08bef6ecd2781b2e61398c827e3015b3062bf1e6
NoahGX <noahjgarcia2019@gmail.com>
2025-01-10 01:34:44 -0800

    Update

---
+++ puzzle.py
@@ -0,0 +1,12 @@
+triangle = [
+    [2],
+    [4, 3],
+    [3, 2, 6],
+    [2, 9, 5, 2],
+    [10, 5, 2, 15, 5]
+]
+
+for row in triangle:
+    for num in row:
+        print(num, end=' ')
+    print()
