881b51103dfbc3952c30376b3b85677c9a2ffd8a
NoahGX <noahjgarcia2019@gmail.com>
2025-01-12 13:57:30 -0800

    Update

--- puzzle.py
+++ puzzle.py
@@ -5,20 +5,14 @@ def pyramid_path(pyramid, target, row=0, idx=0, product=1, path=""):
     # If the last row is reached, check if target is met
     if row == len(pyramid) - 1:
         return path  # valid path found
-    else:
-        return None  # no path found
     
     # Otherwise, try going left
     left = pyramid_path(pyramid, target, row+1, idx, product, path+"L")
-
-    # If path is found while going left, return it
     if left is not None:
         return left
 
     # If left does not succeed, try going right
     right = pyramid_path(pyramid, target, row+1, idx+1, product, path+"R")
-
-    # If path is found while going right, return it
     return right
 
 # Main function
