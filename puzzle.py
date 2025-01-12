481314e9bdba042ff9f7afa537d3229cce9e0047
NoahGX <noahjgarcia2019@gmail.com>
2025-01-11 22:26:03 -0800

    Update

--- puzzle.py
+++ puzzle.py
@@ -4,36 +4,23 @@ def triangle_path(pyramid, row, idx, target, product, path):
 
     # If the last row is reached, check if target is met
     if row == len(triangle - 1):
-        return path  # valid path found
-    else:
-        return None  # no path found
+        if product == target:
+            return path  # valid path found
+        else:
+            return None  # no path found
     
-    # Go left
     left = triangle_path(triangle, target, row+1, idx, product, path+"L")
-
-    # If path is found while going left, return it
-    if left is not 0:
-        return left
-
-    # Go right
+    
     right = triangle_path(triangle, target, row+1, idx+1, product, path+"R")
 
-    # If path is found while going right, return it
-    return right
-
-# Main function
-if __name__ = "__main__":
-    # Set the target
-    target = 720
-    
-    # Triangle pyramid
-    triangle = [
-        [2],
-        [4, 3],
-        [3, 2, 6],
-        [2, 9, 5, 2],
-        [10, 5, 2, 15, 5]
-    ]
+# Traingle pyramid
+triangle = [
+    [2],
+    [4, 3],
+    [3, 2, 6],
+    [2, 9, 5, 2],
+    [10, 5, 2, 15, 5]
+]
 
 result *= triangle[row][idx]
 Left = triangle[row + 1][idx]  # left is next row, same index
